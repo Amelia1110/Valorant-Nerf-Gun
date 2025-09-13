c2786df2d4bc2c88a4cb5f16bffa4c14cc4cafd9
Amelia1110 <ameliasongjiyun@hotmail.com>
2025-09-13 12:27:25 -0400

    Add wifi configuration

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
 
 #define LED D5
+
+// Wifi
+const char* ssid     = "HackTheNorth";
+const char* password = "HTN2025!";
+const char* PC_IP    = "Placeholder"; // TODO: IP of your PC running Python CHANGE THIS
+const int   PC_PORT  = 5005;
+
+WiFiUDP udp;
 
 const int MPU_addr = 0x68; // I2C address
 int16_t a_cX, a_cY, a_cZ, tmp, g_yX, g_yY, g_yZ;
@@ -16,8 +26,13 @@ void setup()
   Wire.write(0);    // set to zero
   Wire.endTransmission(true);
 
-  Serial.begin(115200);
-  Serial.println("ESP8266 MPU6050 Raw Data");
+  // Wi-Fi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+  udp.begin(PC_PORT);
 }
 
 void loop()
@@ -35,22 +50,26 @@ void loop()
   g_yY = Wire.read() << 8 | Wire.read(); // 0x45 GYRO_YOUT_H 0x46 GYRO_YOUT_L
   g_yZ = Wire.read() << 8 | Wire.read(); // 0x47 GYRO_ZOUT_H 0x48 GYRO_ZOUT_L
 
-  Serial.print("Accel: ");
-  Serial.print(a_cX);
-  Serial.print(",");
-  Serial.print(a_cY);
-  Serial.print(",");
-  Serial.print(a_cZ);
+  float ax = a_cX / 16384.0f;
+  float ay = a_cY / 16384.0f;
+  float az = a_cZ / 16384.0f;
 
-  Serial.print(" | Gyro: ");
-  Serial.print(g_yX);
-  Serial.print(",");
-  Serial.print(g_yY);
-  Serial.print(",");
-  Serial.print(g_yZ);
+  float gx = g_yX / 131.0f;
+  float gy = g_yY / 131.0f;
+  float gz = g_yZ / 131.0f;
 
-  Serial.print(" | Temp: ");
-  Serial.println(tmp);
+  // Pack into 24 bytes (6 floats)
+  uint8_t buf[24];
+  memcpy(buf,       &ax, 4);
+  memcpy(buf + 4,   &ay, 4);
+  memcpy(buf + 8,   &az, 4);
+  memcpy(buf + 12,  &gx, 4);
+  memcpy(buf + 16,  &gy, 4);
+  memcpy(buf + 20,  &gz, 4);
+
+  udp.beginPacket(PC_IP, PC_PORT);
+  udp.write(buf, sizeof(buf));
+  udp.endPacket();
 
   digitalWrite(LED, !digitalRead(LED));
 
