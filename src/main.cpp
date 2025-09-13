666e7d37d9968c06a37f042b0778a1666cf62c76
EmChen06 <emileechen95@gmail.com>
2025-09-13 14:33:31 -0400

    adding joystick

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,26 @@
 #include <Arduino.h>
-#define LED D5
+#include <Wire.h>
+#define s1 D4
+#define s2 D3
+
+const int en = 2, rw = 1, rs = 0, d4 = 4, d5 = 5, d6 = 6, d7 = 7, b1 = 3;
 
 void setup() {
-  pinMode(LED,OUTPUT);
+  pinMode(A0, INPUT);
+  digitalWrite(D4, LOW);
+  digitalWrite(D3, LOW);
 }
 
+int varX, varY;
+
 void loop() {
-  delay(1000);
-  digitalWrite(LED,HIGH);
-  delay(1000);
-  digitalWrite(LED,LOW);
+  varX = analogRead(A0);
+  Serial.print(varX);
+  delay(10);
+
+  varY = analogRead(A0);
+  Serial.print(varY);
+  delay(10);
+
+  delay(100);
 }
