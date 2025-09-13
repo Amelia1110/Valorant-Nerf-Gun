6852081d572ef69dca272f52dbf51d136fd239e3
EmChen06 <emileechen95@gmail.com>
2025-09-13 17:32:33 -0400

    walk/run parameters, missing jump

--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,13 @@ void loop() {
   fwd = analogRead(A0);
   jump = digitalRead(s1);
 
-  Serial.print(fwd);
-  Serial.print(" || ");
-  Serial.println(jump); //jump == 0 when pressed
+  // Serial.print(fwd); //542 default
+  // Serial.print(" || ");
+  // Serial.println(jump); //jump == 0 when pressed
+
+  if (fwd < 120) Serial.println("run");
+  else if (fwd >= 120 && fwd < 542) Serial.println("walk");
+  else if (fwd > 542) Serial.println("back walk");
+  
   delay(10);
 }
